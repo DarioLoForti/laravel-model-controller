992d9007514da95c7dcb3e77ecf9904cc67f469f
DarioLoForti <lofortidario@gmail.com>
2024-02-14 17:53:38 +0100

    mostro le carte dei film tramite database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
         <div class="row">
             <div class="col-12">
                 <h1 class="text-center">i miei film</h1>
+                <div class="col-12 py-5">
+                    <a href="{{ route('index_movies') }}" class="btn btn-sm btn-primary ">Vai!</a>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -4,11 +4,25 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <h1>card</h1>
+            <h1 class="text-center text-white py-3">I miei Film</h1>
+            @foreach ($movies as $movie)
+                <div class="col-3 my-3">
+                    <div class="card">
+                        <div class="m-3">
+                            <h4>{{ $movie->title }}</h4>
+                            <h5>Original title: {{ $movie->original_title }}</h5>
+                        </div>
+                        <div class="py-2">
+                            <p>Nationality: {{ $movie->nationality }}</p>
+                        </div>
+                        <div>
+                            <p> Release date: {{ $movie->date }}</p>
+                            <p>Vote: {{ $movie->vote }}</p>
+                        </div>
+                    </div>
+                    </a>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 @endsection
